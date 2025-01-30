688587422fc3987a2d09f02a15598da361458d53
kira25-sys <fontsfelipe77@gmail.com>
2025-01-30 11:54:53 -0300

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,6 +1,3 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
 // Tu configuración de Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyA5rlrZUYRYXTbXLDVXz7o1jSD1gl_Z07M",
@@ -11,13 +8,7 @@ const firebaseConfig = {
   appId: "1:885129941623:web:75cf6364ecd2b7710dec72",
 };
 
-
-// Inicializa Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Exporta o asigna a variables globales (si las necesitas en otras partes)
 const db = firebase.firestore();
 const auth = firebase.auth();
-
-// Opcional: si quieres una referencia al storage, etc.
-// const storage = firebase.storage();
