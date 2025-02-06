bde7ed220bab347afcd7090281e82aada5d14388
kira25-sys <fontsfelipe77@gmail.com>
2025-02-06 16:40:38 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,13 @@
 // Configuración de Firebase
-
+const firebaseConfig = {
+  apiKey: "AIzaSyA5rlrZUYRYXTbXLDVXz7o1jSD1gl_Z07M",
+  authDomain: "chisme-30b9d.firebaseapp.com",
+  projectId: "chisme-30b9d",
+  storageBucket: "chisme-30b9d.firebasestorage.app",
+  messagingSenderId: "885129941623",
+  appId: "1:885129941623:web:75cf6364ecd2b7710dec72",
+  measurementId: "G-YNP0JB5HY9"
+};
 
 // Inicializar Firebase solo si no está ya inicializado
 if (!firebase.apps.length) {
